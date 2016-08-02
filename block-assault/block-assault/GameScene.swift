9120d40ebd34f6fc2663c4865f0f83cd3cf0e712
Edward <edmcintosh85@gmail.com>
2016-08-01 20:13:00 -0600

    First commit

---
+++ block-assault/block-assault/GameScene.swift
@@ -0,0 +1,55 @@
+//
+//  GameScene.swift
+//  block-assault
+//
+//  Created by Edward McIntosh on 2016-08-01.
+//  Copyright (c) 2016 Edward McIntosh. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Hello, World!"
+        myLabel.fontSize = 45
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        
+        self.addChild(myLabel)
+        
+        let myBlock = SKSpriteNode();
+        myBlock.name = "Block";
+        myBlock.size = CGSizeMake(300,100);
+        myBlock.color = SKColor.whiteColor()
+        myBlock.position = CGPoint(x: 100, y: 100)
+        self.addChild(myBlock)
+        
+        
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
