9208defbc6ceffe95a70aa6bfba92282b7d4d8d5
Edward <edmcintosh85@gmail.com>
2016-09-12 21:09:15 -0600

    animation and things happening! Next step is collision detection

--- block-assault/block-assault/GameScene.swift
+++ block-assault/block-assault/GameScene.swift
@@ -8,6 +8,39 @@
 
 import SpriteKit
 
+func + (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func - (left: CGPoint, right: CGPoint) -> CGPoint {
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func * (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func / (point: CGPoint, scalar: CGFloat) -> CGPoint {
+    return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+#if !(arch(x86_64) || arch(arm64))
+    func sqrt(a: CGFloat) -> CGFloat {
+        return CGFloat(sqrtf(Float(a)))
+    }
+#endif
+
+extension CGPoint {
+    func length() -> CGFloat {
+        return sqrt(x*x + y*y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+}
+
+
 class GameScene: SKScene {
     // 1
     let player = SKSpriteNode(imageNamed: "player")
@@ -26,6 +59,47 @@ class GameScene: SKScene {
                 SKAction.waitForDuration(1.0)
                 ])
             ));
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // 1) choose the touch to work with
+        guard let touch = touches.first else{
+            return;
+        }
+        let touchLocation = touch.locationInNode(self);
+        
+        // 2) Set up initial location of projectile
+        
+        let projectile = SKSpriteNode(imageNamed: "projectile");
+        projectile.position = player.position;
+        
+        // 3) determin offset of location to projectile
+        let offset = touchLocation - projectile.position;
+        
+        // 4) Bail out if you are shooting down or backwards
+        if(0.0 > offset.x)
+        {
+            return
+        }
+        
+        // 5) Ok to add now - you've double checked position
+        
+        addChild(projectile);
+        
+        // 6) get the direction of where to shoot
+        let direction = offset.normalized();
+        
+        // 7 Make it shoot far enough to be guaranteed off screen
+        let shootAmount = direction * 1000;
+        
+        // 8) Add the shot amount to the current position
+        let realDest = shootAmount + projectile.position;
+        
+        // 9) Create the action
+        let actionMove = SKAction.moveTo(realDest,duration: 2.0);
+        let actionMoveDone = SKAction.removeFromParent();
+        projectile.runAction(SKAction.sequence([actionMove,actionMoveDone]));
+        
     }
     
     override func update(currentTime: CFTimeInterval) {
