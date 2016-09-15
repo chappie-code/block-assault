dc5a0bb1ddbde9b7d674b2c37528aedaee77ffef
Edward <edmcintosh85@gmail.com>
2016-09-14 23:45:06 -0600

    Added collision detection

--- block-assault/block-assault/GameScene.swift
+++ block-assault/block-assault/GameScene.swift
@@ -8,6 +8,13 @@
 
 import SpriteKit
 
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Monster   : UInt32 = 0b1       // 1
+    static let Projectile: UInt32 = 0b10      // 2
+}
+
 func + (left: CGPoint, right: CGPoint) -> CGPoint {
     return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
@@ -41,11 +48,18 @@ extension CGPoint {
 }
 
 
-class GameScene: SKScene {
+class GameScene: SKScene , SKPhysicsContactDelegate {
     // 1
-    let player = SKSpriteNode(imageNamed: "player")
-    
+    let player = SKSpriteNode(imageNamed: "player");
+        
     override func didMoveToView(view: SKView) {
+        
+        
+        
+        
+        physicsWorld.gravity = CGVectorMake(0, 0);
+        physicsWorld.contactDelegate = self;
+
         // 2
         backgroundColor = SKColor.whiteColor()
         // 3
@@ -72,6 +86,15 @@ class GameScene: SKScene {
         
         let projectile = SKSpriteNode(imageNamed: "projectile");
         projectile.position = player.position;
+        
+        
+        
+        projectile.physicsBody = SKPhysicsBody(circleOfRadius: projectile.size.width/2)
+        projectile.physicsBody?.dynamic = true
+        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Monster
+        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+        projectile.physicsBody?.usesPreciseCollisionDetection = true
         
         // 3) determin offset of location to projectile
         let offset = touchLocation - projectile.position;
@@ -102,6 +125,33 @@ class GameScene: SKScene {
         
     }
     
+    func didBeginContact(contact: SKPhysicsContact) {
+        
+        // 1
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        // 2
+        if ((firstBody.categoryBitMask & PhysicsCategory.Monster != 0) &&
+            (secondBody.categoryBitMask & PhysicsCategory.Projectile != 0)) {
+            projectileDidCollideWithMonster(firstBody.node as! SKSpriteNode, monster: secondBody.node as! SKSpriteNode)
+        }
+        
+    }
+    
+    func projectileDidCollideWithMonster(projectile:SKSpriteNode, monster:SKSpriteNode) {
+        print("Hit")
+        projectile.removeFromParent()
+        monster.removeFromParent()
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
@@ -118,6 +168,13 @@ class GameScene: SKScene {
         
         // Create sprite
         let monster = SKSpriteNode(imageNamed: "monster")
+        
+        monster.physicsBody = SKPhysicsBody(rectangleOfSize: monster.size) // 1
+        monster.physicsBody?.dynamic = true // 2
+        monster.physicsBody?.categoryBitMask = PhysicsCategory.Monster // 3
+        monster.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile // 4
+        monster.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
+        
         
         // Determine where to spawn the monster along the Y axis
         let actualY = random(min: monster.size.height/2, max: size.height - monster.size.height/2)
